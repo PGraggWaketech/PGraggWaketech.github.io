6799ce3d91c1dbbd327486a11291bf0229cc82ca
PGraggWaketech <pbgragg@my.waketech.edu>
2025-04-11 21:54:56 -0400

    Added gradebook html and js files

---
+++ gradebook.js
@@ -0,0 +1,10 @@
+function fetchGradeData() {
+	console.log("fetching grade data...");
+}
+
+function populateGradebook (data) {
+	console.log("populating gradebook with data:", data);
+}
+
+const gradeData = fetchGradeData();
+populateGradebook(gradeData);
